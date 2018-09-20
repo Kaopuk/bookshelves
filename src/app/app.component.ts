3da718271c23afd548560fcf356704df2bc050d2
Kaopuk <baptiste.lavignon@gmail.com>
2018-09-20 13:23:44 +0200

    firebase lié a l'application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-
+import * as firebase from 'firebase';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bookshelves';
+  constructor() {
+    var config = {
+      apiKey: "AIzaSyCNMElYdnn7owqvMZm_Ycnna49-z9oObzM",
+      authDomain: "bookshelves-kaopuk.firebaseapp.com",
+      databaseURL: "https://bookshelves-kaopuk.firebaseio.com",
+      projectId: "bookshelves-kaopuk",
+      storageBucket: "bookshelves-kaopuk.appspot.com",
+      messagingSenderId: "6496419940"
+    };
+    firebase.initializeApp(config);
+  }
 }
