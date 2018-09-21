d44c2bff65f6a3a6d2ef242752258dece8b0ff32
Kaopuk <baptiste.lavignon@gmail.com>
2018-09-21 11:50:49 +0200

    mise en plce des book list book single book

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import * as firebase from 'firebase';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,13 +8,13 @@ import * as firebase from 'firebase';
 })
 export class AppComponent {
   constructor() {
-    var config = {
-      apiKey: "AIzaSyCNMElYdnn7owqvMZm_Ycnna49-z9oObzM",
-      authDomain: "bookshelves-kaopuk.firebaseapp.com",
-      databaseURL: "https://bookshelves-kaopuk.firebaseio.com",
-      projectId: "bookshelves-kaopuk",
-      storageBucket: "bookshelves-kaopuk.appspot.com",
-      messagingSenderId: "6496419940"
+    const config = {
+      apiKey: 'AIzaSyCNMElYdnn7owqvMZm_Ycnna49-z9oObzM',
+      authDomain: 'bookshelves-kaopuk.firebaseapp.com',
+      databaseURL: 'https://bookshelves-kaopuk.firebaseio.com',
+      projectId: 'bookshelves-kaopuk',
+      storageBucket: 'bookshelves-kaopuk.appspot.com',
+      messagingSenderId: '6496419940'
     };
     firebase.initializeApp(config);
   }
